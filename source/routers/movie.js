cc5c814eabc5e7e9334703c7a543badb3bda9a4c
Nicolas Quintero <nicolasquintero151@gmail.com>
2022-12-07 13:30:33 -0500

    proyecto peliculas digital house

---
+++ source/routers/movie.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const rutas = express.Router();
+const controllerMovie = require('../controllers/movie')
+const validaciones = require('../validations/validation-create-movie')
+rutas.get('/', controllerMovie.getAllMovies);
+rutas.get('/movie/:id', controllerMovie.getByIdMovies);
+rutas.get('/gestion-pelicula', controllerMovie.gestionPelicula);
+rutas.get('/editar-pelicula/:id', controllerMovie.editarPelicula);
+rutas.post('/crear-pelicula', validaciones, controllerMovie.createMovie);
+rutas.get('/movie-detele/:id', controllerMovie.deleteMovie);
+
+//exportando el modulo de rutas
+module.exports = rutas;
